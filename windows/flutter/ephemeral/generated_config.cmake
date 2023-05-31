9f4e102967c7a566d5f7a3a2271e68180051bb1d
Mochammad Rachmadani <64763359+rachmaddani66@users.noreply.github.com>
2023-05-31 14:11:49 +0700

    Basic_Widget

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\Mochammad Rachmadani\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Mochammad Rachmadani\\Desktop\\loginpageFlutter-main\\Page" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\Mochammad Rachmadani\\flutter"
+  "PROJECT_DIR=C:\\Users\\Mochammad Rachmadani\\Desktop\\loginpageFlutter-main\\Page"
+  "FLUTTER_ROOT=C:\\Users\\Mochammad Rachmadani\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Mochammad Rachmadani\\Desktop\\loginpageFlutter-main\\Page\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Mochammad Rachmadani\\Desktop\\loginpageFlutter-main\\Page"
+  "FLUTTER_TARGET=C:\\Users\\Mochammad Rachmadani\\Desktop\\loginpageFlutter-main\\Page\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Mochammad Rachmadani\\Desktop\\loginpageFlutter-main\\Page\\.dart_tool\\package_config.json"
+)
